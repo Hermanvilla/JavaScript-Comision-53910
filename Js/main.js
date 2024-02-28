351f419e9c84c8e4c58083af8b19ed68d47f3bd1
Hermanvilla <hjvillalobosl@gmail.com>
2024-02-27 23:18:58 -0300

    Version 2da entrega

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,5 @@
 /* 
+Codigo Usando comandos basicos
 
 let ingresar = false;
 let contacto;
@@ -96,7 +97,12 @@ while (opcion !== "X") {
 
 **/
 
+
+/**  
+Version Primera entrega con Funciones
+
 let nombre = prompt("Ingresa tu nombre");
+
 
 function contactarAbogado(nombreAbogado) {
     let contacto;
@@ -154,7 +160,103 @@ function mainMenu() {
                 break; // Cierra el loop
             default:
                 alert("Opción inválida");
+        } 
+    }
+}
+
+mainMenu();
+*/
+
+
+let nombre = prompt("Ingresa tu nombre");
+
+// Definiendo los abogados y sus especialidades en un array de objetos
+
+let abogados = [
+    { nombre: "Marcos Linares", especialidad: "Civil" },
+    { nombre: "Eduardo Urdaneta", especialidad: "Laboral" },
+    { nombre: "Natalia Cuomo", especialidad: "Intelectual" },
+    { nombre: "Jose Hernandez", especialidad: "Laboral" },
+    { nombre: "Luis Gonzalez", especialidad: "Penal" },
+    { nombre: "Javiera Jimenez", especialidad: "Migratorio" },
+    { nombre: "Jesus Arellano", especialidad: "Civil" },
+    { nombre: "Natalia Cuomo", especialidad: "Intelectual" },
+    { nombre: "Equipo de Especialistas", especialidad: "Otro" }
+];
+
+function contactarAbogado(abogado) {
+    let contacto;
+    while (contacto !== "4") {
+        contacto = prompt(`¿Cómo contactarte con ${abogado.nombre}? \n1. Whatsapp \n2. Email  \n3. Agenda una llamada \n4. Regresa al menu anterior`);
+        switch (contacto) {
+            case "1":
+                alert("Seleccionaste Whatsapp");
+                break;
+            case "2":
+                alert("Seleccionaste Email");
+                break;
+            case "3":
+                alert("Agenda tu llamada acá");
+                break;
+            case "4":
+                alert("Regresando al menu anterior");
+                break;
+            default:
+                alert("Opción inválida");
         }
+    }
+}
+
+function filtrarPorEspecialidad(especialidad) {
+    return abogados.filter(abogado => abogado.especialidad === especialidad);
+}
+
+function mainMenu() {
+    let opcion;
+    while (opcion !== "x") {
+        opcion = prompt(`
+            \n¿Cómo podemos ayudarte? 
+            Civil 
+            Laboral 
+            Intelectual
+            Migratorio
+            Penal 
+            Otro 
+            Presiona "x" para salir
+        `);
+
+        switch (opcion) {
+            case "Civil":
+            case "Laboral":
+            case "Intelectual":
+            case "Penal":
+            case "Migratorio":
+            case "Otro":
+                let abogadosEspecialidad = filtrarPorEspecialidad(opcion);
+                if (abogadosEspecialidad.length > 0) {
+                    if (abogadosEspecialidad.length === 1) {
+                        alert("Hola " + nombre + " El Abogado " + abogadosEspecialidad[0].nombre + " será tu asesor especializado");
+                        contactarAbogado(abogadosEspecialidad[0]);
+                    } else {
+                        let seleccion = prompt(`Hola ${nombre}, se han encontrado ${abogadosEspecialidad.length} abogados disponibles para esta especialidad. Por favor selecciona con quién deseas agendar:\n${abogadosEspecialidad.map((abogado, index) => `${index + 1}. ${abogado.nombre}`).join("\n")}`);
+                        seleccion = parseInt(seleccion);
+                        if (!isNaN(seleccion) && seleccion >= 1 && seleccion <= abogadosEspecialidad.length) {
+                            alert("Seleccionaste al Abogado " + abogadosEspecialidad[seleccion - 1].nombre + " para atender tu solicitud");
+                            contactarAbogado(abogadosEspecialidad[seleccion - 1]);
+                        break;
+                        } else {
+                            alert("Selección inválida.");
+                        }
+                    }
+                } else {
+                    alert("Lo sentimos, no hay abogados disponibles para esta especialidad.");
+                }
+                break;
+            case "X":
+                break; // Cierra el loop
+            default:
+                alert("Si tu requerimiento no se encuentra en nuestra lista selecciona Otro");
+        } 
     }
 }
 
